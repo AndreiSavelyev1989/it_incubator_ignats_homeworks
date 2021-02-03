bc6c2cac485cc3cdfb59fbda4233db7e431eafad
Andrei <andreiscorpion19892010@gmail.com>
2021-02-03 14:51:30 +0200

    Updates

---
+++ precache-manifest.5266ed72d07398299b4f6f16e60205c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "198a3f0a33862b5872781758fa34a992",
+    "url": "/it_incubator_ignats_homeworks/index.html"
+  },
+  {
+    "revision": "1c5a651b7f6e79766e58",
+    "url": "/it_incubator_ignats_homeworks/static/css/main.20e19243.chunk.css"
+  },
+  {
+    "revision": "0655d7e84ffcbd934d76",
+    "url": "/it_incubator_ignats_homeworks/static/js/2.86d40d7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/it_incubator_ignats_homeworks/static/js/2.86d40d7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c5a651b7f6e79766e58",
+    "url": "/it_incubator_ignats_homeworks/static/js/main.fd880d51.chunk.js"
+  },
+  {
+    "revision": "a082c198d0caefe8a310",
+    "url": "/it_incubator_ignats_homeworks/static/js/runtime-main.20dd47a7.js"
+  },
+  {
+    "revision": "22737a7c8d8867c22b0b06c89582544b",
+    "url": "/it_incubator_ignats_homeworks/static/media/messageAvatar.22737a7c.png"
+  }
+]);
